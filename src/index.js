ec7f3aa342f1bf4aeca70a389f0c8c1d89ad12f9
Joe Kim <josephkim0224@gmail.com>
2017-10-19 12:40:52 -0400

    installed redux and react-redux.  Also updated main index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import RootContainer from './containers/RootContainer';
+import configureStore from './store/configureStore';
+
+
+const render = (Component) => {
+  ReactDOM.render(
+    <Provider store={configureStore()}>
+      <Component />
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+render(RootContainer);
